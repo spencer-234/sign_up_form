a9e24209852fadf7d23a6591baba19de63e66ba8
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-08-28 17:57:31 -0400

    change citation link with theme buttons

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const rootElement = document.documentElement;
-const logoImg = document.querySelector(".logo_img");
-const citation = document.querySelector(".citation");
+const citation1 = document.querySelector(".citation1");
+const citation2 = document.querySelector(".citation2");
 
 rootElement.classList.add("dark");
 
 function light() {
     rootElement.classList.remove("dark");
     rootElement.classList.add("light");
+    citation1.setAttribute("style", "display: none");
+    citation2.setAttribute("style", "display: block");
+    
 }
 
 function dark() {
     rootElement.classList.remove("light");
     rootElement.classList.add("dark");
+    citation1.setAttribute("style", "display: block");
+    citation2.setAttribute("style", "display: none");
 }
 
