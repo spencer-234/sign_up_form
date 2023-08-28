a3e9e1b38362b05ba5bdfe2ada7bc2897b19db8c
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-08-28 14:12:44 -0400

    add functionality to theme buttons

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const rootElement = document.documentElement;
+const logoImg = document.querySelector(".logo_img");
+
 rootElement.classList.add("dark");
+
+function light() {
+    rootElement.classList.remove("dark");
+    rootElement.classList.add("light");
+}
+
+function dark() {
+    rootElement.classList.remove("light");
+    rootElement.classList.add("dark");
+}
+
