af60917de5440c0ceab7567e40e2e7744be374a8
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-08-29 12:10:59 -0400

    add password confirmation function

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ const citation2 = document.querySelector(".citation2");
 
 rootElement.classList.add("dark");
 
+confirmation.addEventListener("input", confirmPassword);
+
 function light() {
     rootElement.classList.remove("dark");
     rootElement.classList.add("light");
     citation1.setAttribute("style", "display: none");
     citation2.setAttribute("style", "display: block");
     
-}
+};
 
 function dark() {
     rootElement.classList.remove("light");
     rootElement.classList.add("dark");
     citation1.setAttribute("style", "display: block");
     citation2.setAttribute("style", "display: none");
-}
+};
 
+function confirmPassword() {
+    let password = document.querySelector("#password");
+    let confirmation = document.querySelector("#confirmation");
+    if (password.value !== confirmation.value) {
+        confirmation.setCustomValidity("Passwords do not match");
+    } else {
+        confirmation.setCustomValidity("");
+    }
+};
